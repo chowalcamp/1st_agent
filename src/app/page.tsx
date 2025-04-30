d5a964e1ffd9b5d64ae055cecef844ad9862636e
FunctionalFactory <178769972+FunctionalFactory@users.noreply.github.com>
2025-04-30 06:31:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero'
+import Features from './components/Features'
+
+export default function Home() {
+  return (
+    <main className="main-content">
+      <Hero />
+      <Features />
+    </main>
+  )
+}
